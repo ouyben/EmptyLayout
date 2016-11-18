9019c812443ada7431de1ca180a213c1a20ba134
ouyben <ouyben@foxmail.com>
2016-11-18 11:40:44 +0800

    第一次提交,

---
+++ Empty/src/main/java/com/ouyben/empty/EmptyLayout.java
@@ -0,0 +1,200 @@
+package com.ouyben.empty;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * 各种状态控制显示
+ */
+public class EmptyLayout extends FrameLayout {
+
+    private Context mContext;
+    private View mEmptyView;// 空布局
+    private View mBindView;//绑定的布局
+    private View mErrorView;// 加载错误
+    private View mLoadingView;// 加载布局
+    private View mNoNetwork;//无网络布局
+
+    public EmptyLayout(Context context) {
+        this(context, null);
+    }
+
+    public EmptyLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EmptyLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.mContext = context;
+
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        //居中
+        params.gravity = Gravity.CENTER;
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.EmptyLayout, 0, 0);
+
+        //数据为空时的布局
+        int emptyLayout = ta.getResourceId(R.styleable.EmptyLayout_elEmptyLayout, R.layout.layout_empty);
+        mEmptyView = View.inflate(context, emptyLayout, null);
+        addView(mEmptyView, params);
+
+        //加载中的布局
+        int loadingLayout = ta.getResourceId(R.styleable.EmptyLayout_elLoadingLayout, R.layout.layout_loading);
+        mLoadingView = View.inflate(context, loadingLayout, null);
+        addView(mLoadingView, params);
+
+        //错误时的布局
+        int errorLayout = ta.getResourceId(R.styleable.EmptyLayout_elErrorLayout, R.layout.layout_error);
+        mErrorView = View.inflate(context, errorLayout, null);
+        addView(mErrorView, params);
+
+        int noNetworkLayout = ta.getResourceId(R.styleable.EmptyLayout_elErrorLayout, R.layout.layout_not_network);
+        mNoNetwork = View.inflate(context, noNetworkLayout, null);
+        addView(mNoNetwork, params);
+        //全部隐藏
+        setGone();
+    }
+
+    /**
+     * TODO: 设置空数据布局
+     *
+     * @param resId
+     */
+    public void setEmptyView(int resId) {
+        setEmptyView(View.inflate(mContext, resId, null));
+    }
+
+    /**
+     * TODO: 设置空数据布局
+     */
+    public void setEmptyView(View v) {
+        if (indexOfChild(mEmptyView) != -1) {
+            removeView(mEmptyView);
+        }
+        mEmptyView = v;
+        addView(mEmptyView);
+        setGone();
+    }
+
+    /**
+     * TODO: 设置加载中布局
+     *
+     * @param layoutId
+     */
+    public void setLoadingView(int layoutId) {
+        setLoadingView(View.inflate(mContext, layoutId, null));
+    }
+
+    /**
+     * todo:设置加载中布局
+     *
+     * @param loadingView
+     */
+    public void setLoadingView(View loadingView) {
+        if (indexOfChild(mLoadingView) != -1) {
+            removeView(mLoadingView);
+        }
+        mLoadingView = loadingView;
+        addView(mLoadingView);
+        setGone();
+    }
+
+    /**
+     * TODO: 设置加载错误布局
+     *
+     * @param layoutId
+     */
+    public void setErrorView(int layoutId) {
+        setLoadingView(View.inflate(mContext, layoutId, null));
+    }
+
+    /**
+     * TODO: 设置加载错误布局
+     *
+     * @param errorView
+     */
+    public void setErrorView(View errorView) {
+        if (indexOfChild(mErrorView) != -1) {
+            removeView(mErrorView);
+        }
+        mErrorView = errorView;
+        addView(mLoadingView);
+        setGone();
+    }
+
+    /**
+     * TODO: 设置无网络布局
+     *
+     * @param layoutId
+     */
+    public void setNoNetworkView(int layoutId) {
+        setLoadingView(View.inflate(mContext, layoutId, null));
+    }
+
+    /**
+     * TODO: 设置无网络布局
+     *
+     * @param view
+     */
+    public void setNoNetworkView(View view) {
+        if (indexOfChild(mNoNetwork) != -1) {
+            removeView(mNoNetwork);
+        }
+        mNoNetwork = view;
+        addView(mNoNetwork);
+        setGone();
+    }
+
+    public void bindView(View view) {
+        mBindView = view;
+    }
+
+    public void showEmpty() {
+        if (mBindView != null)
+            mBindView.setVisibility(View.GONE);
+        setGone();
+        mEmptyView.setVisibility(View.VISIBLE);
+    }
+
+    public void showError() {
+        if (mBindView != null)
+            mBindView.setVisibility(View.GONE);
+        setGone();
+        mErrorView.setVisibility(View.VISIBLE);
+    }
+
+    public void showLoading() {
+        if (mBindView != null)
+            mBindView.setVisibility(View.GONE);
+        setGone();
+        mLoadingView.setVisibility(View.VISIBLE);
+    }
+
+    public void showNoNetwork() {
+        if (mBindView != null)
+            mBindView.setVisibility(View.GONE);
+        setGone();
+        mNoNetwork.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 全部隐藏
+     */
+    private void setGone() {
+        mEmptyView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.GONE);
+        mLoadingView.setVisibility(View.GONE);
+        mNoNetwork.setVisibility(View.GONE);
+    }
+
+    public void showSuccess() {
+        if (mBindView != null)
+            mBindView.setVisibility(View.VISIBLE);
+        setGone();
+    }
+
+}
---
+++ app/src/main/java/com/ouyben/emptylayout/MainActivity.java
@@ -0,0 +1,83 @@
+package com.ouyben.emptylayout;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ouyben.empty.EmptyLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar mToolbar;
+    private TextView mTv;
+    private EmptyLayout mEmpty;
+    private RelativeLayout mContentMain;
+    private FloatingActionButton mFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_no_net) {//无网络
+            mEmpty.showNoNetwork();
+            return true;
+        } else if (id == R.id.action_error) {
+            mEmpty.showError();
+            return true;
+        } else if (id == R.id.action_load) {
+            mEmpty.showLoading();
+            return true;
+        } else if (id == R.id.action_no_data) {
+            mEmpty.showEmpty();
+            return true;
+        } else if (id == R.id.action_sucess) {
+            mEmpty.showSuccess();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTv = (TextView) findViewById(R.id.tv);
+        mEmpty = (EmptyLayout) findViewById(R.id.empty);
+        mContentMain = (RelativeLayout) findViewById(R.id.content_main);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        mFab.setOnClickListener(this);
+
+        mToolbar.setTitle("状态显示");
+        setSupportActionBar(mToolbar);
+        mEmpty.bindView(mTv);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                mEmpty.showSuccess();
+                break;
+        }
+    }
+}
